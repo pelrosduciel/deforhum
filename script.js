504b31ee1eb6851917da2b85044ae0399f45d770
elros duciel <princeelrosartist@gmail.com>
2024-04-09 07:26:36 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelector('h1').addEventListener('click', () => {
+        alert('Bonjour ! Tu as cliqué sur le titre.');
+    });
+});
